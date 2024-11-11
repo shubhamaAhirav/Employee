95206b03a6bc8f0fce021bf1c437bb35afa365cf
shubhamaAhirav <shubham.ashirav@devputers.com>
2024-11-11 16:41:40 +0530

    Main File

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from .db.connection import Base, engine
 from .routes import employee
 
-# Create all tables in the database
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
