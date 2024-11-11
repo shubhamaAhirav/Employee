cf39e7e8d03bd71c8200b4f0d525d428cb351fd6
shubhamaAhirav <shubham.ashirav@devputers.com>
2024-11-11 16:40:02 +0530

    Services code

---
+++ services/employee_service.py
@@ -0,0 +1,25 @@
+from sqlalchemy.orm import Session
+from ..models import employee as models
+from ..schemas import employee as schemas
+
+def get_employee(db: Session, employee_id: int):
+    return db.query(models.Employee).filter(models.Employee.id == employee_id).first()
+
+def get_employees(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(models.Employee).offset(skip).limit(limit).all()
+
+def create_employee(db: Session, employee: schemas.EmployeeCreate):
+    db_employee = models.Employee(
+        name=employee.name, age=employee.age, position=employee.position, department=employee.department
+    )
+    db.add(db_employee)
+    db.commit()
+    db.refresh(db_employee)
+    return db_employee
+
+def delete_employee(db: Session, employee_id: int):
+    employee = db.query(models.Employee).filter(models.Employee.id == employee_id).first()
+    if employee:
+        db.delete(employee)
+        db.commit()
+    return employee
