f6ed147aa2e8387c3ef584b4fdfad97f1b188c4f
shubhamaAhirav <shubham.ashirav@devputers.com>
2024-11-11 16:40:30 +0530

    Routes code

---
+++ routes/employee.py
@@ -0,0 +1,37 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from ..schemas import employee as schemas
+from ..services import employee_service as service
+from ..db.connection import SessionLocal
+
+router = APIRouter()
+
+# Dependency to get DB session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.post("/", response_model=schemas.Employee)
+def create_employee(employee: schemas.EmployeeCreate, db: Session = Depends(get_db)):
+    return service.create_employee(db=db, employee=employee)
+
+@router.get("/{employee_id}", response_model=schemas.Employee)
+def read_employee(employee_id: int, db: Session = Depends(get_db)):
+    db_employee = service.get_employee(db, employee_id=employee_id)
+    if db_employee is None:
+        raise HTTPException(status_code=404, detail="Employee not found")
+    return db_employee
+
+@router.get("/", response_model=list[schemas.Employee])
+def read_employees(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    return service.get_employees(db, skip=skip, limit=limit)
+
+@router.delete("/{employee_id}", response_model=schemas.Employee)
+def delete_employee(employee_id: int, db: Session = Depends(get_db)):
+    db_employee = service.delete_employee(db, employee_id=employee_id)
+    if db_employee is None:
+        raise HTTPException(status_code=404, detail="Employee not found")
+    return db_employee
